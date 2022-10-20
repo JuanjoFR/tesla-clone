4ee784c76e24ea3c573f1d26b4cf16f8bd8a44c7
Juanjo Fernández <juanjo.fernandez@haa.st>
2022-10-20 17:25:08 +0200

    Add scroll snap

--- pattern-library/templates/home.tsx
+++ pattern-library/templates/home.tsx
@@ -1,7 +1,7 @@
-import Button from "../atoms/Button"
-import Title from "../atoms/Title"
+import Button from "../atoms/button"
 import Label from "../atoms/label"
 import Subtitle from "../atoms/subtitle"
+import Title from "../atoms/title"
 import React from "react"
 
 interface Action {
@@ -22,37 +22,36 @@ interface Props {
 
 function Home({ sections }: Props) {
   return (
-    <React.Fragment>
+    <div className="snap-y snap-mandatory h-screen overflow-scroll">
       {sections.map((section) => (
-        <div key={section.id} className="overflow-hidden">
-          <div
-            style={{
-              backgroundImage: `url(${section.backgroundImageUrl})`
-            }}
-            className="flex flex-col h-screen bg-center bg-cover items-center justify-between text-center pt-28 pb-14"
-          >
-            <div>
-              <Title text={section.title} />
-              {section.subtitle ? (
-                <Subtitle text={section.subtitle} />
-              ) : undefined}
+        <div
+          key={section.id}
+          style={{
+            backgroundImage: `url(${section.backgroundImageUrl})`
+          }}
+          className="snap-center w-screen h-screen flex flex-col justify-between items-center bg-center bg-cover pt-28 pb-14"
+        >
+          <div className="flex flex-col items-center">
+            <Title text={section.title} />
+            {section.subtitle ? (
+              <Subtitle text={section.subtitle} />
+            ) : undefined}
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="flex flex-row gap-8 mb-8">
+              {section.actions.map((action, index) => (
+                <Button
+                  key={index.toString()}
+                  title={action.title}
+                  variant={action.variant}
+                />
+              ))}
             </div>
-            <div className="flex flex-col items-center">
-              <div className="flex flex-row gap-8 mb-8">
-                {section.actions.map((action, index) => (
-                  <Button
-                    key={index.toString()}
-                    title={action.title}
-                    variant={action.variant}
-                  />
-                ))}
-              </div>
-              {section.info ? <Label text={section.info} /> : undefined}
-            </div>
+            {section.info ? <Label text={section.info} /> : undefined}
           </div>
         </div>
       ))}
-    </React.Fragment>
+    </div>
   )
 }
 
